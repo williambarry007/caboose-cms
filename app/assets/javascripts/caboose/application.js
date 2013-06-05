b91a5a6da053004fdd3086ebcfc1a6b3ecf4f27c
William Barry <williambarry007@gmail.com>
2013-06-05 10:01:03 -0500

    More changes

--- app/assets/javascripts/caboose/application.js
+++ app/assets/javascripts/caboose/application.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require caboose_before
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
@@ -32,7 +31,8 @@
 //= require attribute/select
 //= require attribute/texarea
 //= require attribute/textjs
+//= require attribute/h1
 //= require attribute/time
 //= require attribute/video
 //= require caboose/station
-//= require caboose_after
+//= require application
--- lib/caboose/version.rb
+++ lib/caboose/version.rb
@@ -1,3 +1,3 @@
 module Caboose
-  VERSION = '0.1.35'
+  VERSION = '0.1.36'
 end

043ef44a803360c6035d003e7f6ef14513e87d92
William Barry <williambarry007@gmail.com>
2013-08-22 15:28:21 -0500

    Fixed bug in authenticator

--- app/models/caboose/authenticator.rb
+++ app/models/caboose/authenticator.rb
@@ -5,7 +5,7 @@ class Caboose::Authenticator
     pass = Digest::SHA1.hexdigest(Caboose::salt + password)
     user = Caboose::User.where(:username => username, :password => pass).first
     if (user.nil?)
-      user = User.where(:email => username, :password => pass).first
+      user = Caboose::User.where(:email => username, :password => pass).first
     end
     return false if user.nil?
     return user       

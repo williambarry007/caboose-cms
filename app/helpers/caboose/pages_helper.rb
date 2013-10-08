213c4c2418bf69615ba62f4f067bd27394e8da9d
William Barry <williambarry007@gmail.com>
2013-10-08 11:16:33 -0500

    More changes

---
+++ app/helpers/caboose/pages_helper.rb
@@ -0,0 +1,27 @@
+module Caboose
+  module PagesHelper
+    
+    def pages_list(page)
+      str = "<ul>"
+      page.children.each do |p|
+        str << pages_list_helper(p)
+      end
+      str << "</ul>"
+      return str
+    end
+    
+    def pages_list_helper(page)
+      str = "<li><a href='/admin/pages/#{page.id}/edit'>#{page.title}</a>"
+      if page.children && page.children.count > 0
+        str << "<ul>"
+        page.children.each do |p|
+          str << pages_list_helper(p)
+        end
+        str << "</ul>"
+      end
+      str << "</li>"
+      return str
+    end
+    
+  end
+end

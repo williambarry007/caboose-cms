5c0df2af9acf9d933ddf425066a059eed7534b47
William Barry <williambarry007@gmail.com>
2013-10-14 11:05:03 -0500

    Changed main pages link in admin to non-modal /admin/pages

--- app/models/caboose/page_plugin.rb
+++ app/models/caboose/page_plugin.rb
@@ -6,23 +6,25 @@ class Caboose::PagePlugin < Caboose::CaboosePlugin
     item = {
       'id' => 'pages', 
       'text' => 'Pages',
-      'children' => []
+      'children' => [],
+      'href' => '/admin/pages',
+      'modal' => false
       #'show_children_default' => true
     }
       
-    is_admin = user.is_allowed('all', 'all')
-    actions = Caboose::Page.permissible_actions(user.id, page.id)
-    if (actions.include?('edit') || is_admin)
-    	item['children'] << { 'href' => "/pages/#{page.id}/sitemap"       , 'text' => 'Site Map This Page'  }
-    	item['children'] << { 'href' => "/pages/#{page.id}/edit"          , 'text' => 'Edit Page Content'   }
-    	item['children'] << { 'href' => "/pages/#{page.id}/edit-settings" , 'text' => 'Edit Page Settings', 'modal' => false }
-
-    	#uru = session['use_redirect_urls'].nil? ? true : session['use_redirect_urls']
-    	#item['children'] << { 'id' => 'use_redirect_urls', 'href' => '#', 'text' => '' + (uru ? 'Disable' : 'Enable') + ' Redirect Urls' }
-    end
-    if (user.is_allowed('pages', 'add') || is_admin)
-      item['children'] << { 'href' => "/pages/new?parent_id=#{page.id}"  , 'text' => 'New Page', 'modal' => true }
-    end    
+    #is_admin = user.is_allowed('all', 'all')
+    #actions = Caboose::Page.permissible_actions(user.id, page.id)
+    #if (actions.include?('edit') || is_admin)
+    #	item['children'] << { 'href' => "/pages/#{page.id}/sitemap"       , 'text' => 'Site Map This Page'  }
+    #	item['children'] << { 'href' => "/pages/#{page.id}/edit"          , 'text' => 'Edit Page Content'   }
+    #	item['children'] << { 'href' => "/pages/#{page.id}/edit-settings" , 'text' => 'Edit Page Settings', 'modal' => false }
+    #
+    #	#uru = session['use_redirect_urls'].nil? ? true : session['use_redirect_urls']
+    #	#item['children'] << { 'id' => 'use_redirect_urls', 'href' => '#', 'text' => '' + (uru ? 'Disable' : 'Enable') + ' Redirect Urls' }
+    #end
+    #if (user.is_allowed('pages', 'add') || is_admin)
+    #  item['children'] << { 'href' => "/pages/new?parent_id=#{page.id}"  , 'text' => 'New Page', 'modal' => true }
+    #end    
     nav << item
     return nav
   end

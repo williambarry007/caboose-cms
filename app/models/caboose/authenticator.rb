d71b07079b1fdca6a018b0d68cf28a305259a2ea
William Barry <williambarry007@gmail.com>
2014-02-14 09:30:27 -0600

    More changes

--- app/models/caboose/authenticator.rb
+++ app/models/caboose/authenticator.rb
@@ -2,13 +2,17 @@
 class Caboose::Authenticator
 
   def authenticate(username, password)
+    resp = Caboose::StdClass.new(
+      'error' => nil,
+      'user' => nil 
+    )
     pass = Digest::SHA1.hexdigest(Caboose::salt + password)
-    user = Caboose::User.where(:username => username, :password => pass).first
-    if (user.nil?)
-      user = Caboose::User.where(:email => username, :password => pass).first
+    resp.user = Caboose::User.where(:username => username, :password => pass).first
+    if (resp.user.nil?)
+      resp.user = Caboose::User.where(:email => username, :password => pass).first
     end
-    return false if user.nil?
-    return user       
+    resp.error = "Invalid credentials" if resp.user.nil?      
+    return resp
   end
   
 end

2d313a4efaa012aa1e61bcacc836267e99c80b16
William Barry <williambarry007@gmail.com>
2014-03-20 11:08:23 -0500

    More changes

--- app/models/caboose/setting.rb
+++ app/models/caboose/setting.rb
@@ -2,4 +2,10 @@
 class Caboose::Setting < ActiveRecord::Base
   self.table_name = "settings"
   attr_accessible :name, :value
+  
+  def self.value_for(name)
+    s = self.where(:name => name).first
+    return '' if s.nil?
+    return s.value
+  end      
 end

3df0eab5315a814a27c14f9086b4a54495b30602
William Barry <williambarry007@gmail.com>
2014-05-23 10:30:55 -0500

    More changes

--- lib/sample_files/config/initializers/caboose.rb
+++ lib/sample_files/config/initializers/caboose.rb
@@ -1,19 +1,19 @@
 
 # Salt to ensure passwords are encrypted securely
-Caboose::salt = '79f15ebc0541f4942dc7fb5fa3a27fe96a255c12'
+Caboose::salt = '|CABOOSE_SALT|'
 
 # Where page asset files will be uploaded
 Caboose::assets_path = Rails.root.join('app', 'assets', 'caboose')
 
 # Register any caboose plugins
-Caboose::plugins << 'RepconnexPlugin'
+#Caboose::plugins << 'MyPlugin'
 
 Caboose::use_url_params = false
 
-Caboose::website_name = "RepConnex"
-Caboose::website_domain = "http://tampa.repconnex.com"
-Caboose::cdn_domain = "//d3w3nonj6twazr.cloudfront.net"
-Caboose::email_from = "contact@repconnex.com"
-Caboose::authenticator_class = 'Authenticator'
+Caboose::website_name = "My Website"
+Caboose::website_domain = "http://mywebsite.com"
+Caboose::cdn_domain = "|CDN_URL|"
+Caboose::email_from = "contact@mywebsite.com"
+#Caboose::authenticator_class = 'Authenticator'
 Caboose::use_ab_testing = false
 Caboose::session_length = 24 # hours

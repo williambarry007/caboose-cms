c760e310dba80b8b0319d2426f14691fd328f565
William Barry <williambarry007@gmail.com>
2014-06-27 14:15:06 -0500

    Changed redirects for quicker routing to page content

--- app/controllers/caboose/admin_controller.rb
+++ app/controllers/caboose/admin_controller.rb
@@ -4,6 +4,7 @@ module Caboose
       
     # GET /admin
     def index
+      redirect_to '/admin/pages' if logged_in?
       @return_url = params[:return_url].nil? ? '/station' : params[:return_url]
     end
     
--- app/helpers/caboose/pages_helper.rb
+++ app/helpers/caboose/pages_helper.rb
@@ -8,7 +8,7 @@ module Caboose
     end
     
     def pages_list_helper(page)
-      str = "<li><a href='/admin/pages/#{page.id}/edit'>#{page.title}</a>"
+      str = "<li><a href='/admin/pages/#{page.id}/content'>#{page.title}</a>"
       if page.children && page.children.count > 0
         str << "<ul>"
         page.children.each do |p|

8914a0e7c693a6b5f04edaee64f207c184795f58
William Barry <williambarry007@gmail.com>
2015-02-06 10:47:32 -0600

    More changes

--- app/controllers/caboose/checkout_controller.rb
+++ app/controllers/caboose/checkout_controller.rb
@@ -83,7 +83,11 @@ module Caboose
             #:relay_url => "#{request.protocol}#{request.host_with_port}/checkout/authnet-relay/#{@order.id}",
             :relay_response => 'TRUE',
             :relay_url => "#{request.protocol}#{request.host_with_port}/checkout/authnet-relay",
-            :transaction_type => 'AUTH_ONLY',            
+            :transaction_type => 'AUTH_ONLY',
+            :billing_address => AuthorizeNet::Address.new(
+              :first_name => 'John', 
+              :last_name => 'Doe'
+            )            
             :test => sc.pp_testing
           )
           @request = request

e844793fba766ef38c15f3590b3034d9d49f7a13
William Barry <williambarry007@gmail.com>
2015-02-10 17:29:44 -0600

    More changes

--- app/controllers/caboose/application_controller.rb
+++ app/controllers/caboose/application_controller.rb
@@ -63,7 +63,8 @@ module Caboose
         create_new_order = true                         
       end
               
-      if create_new_order # Create an order to associate with the session                
+      if create_new_order # Create an order to associate with the session
+        Caboose.log(@order)
         @order = Caboose::Order.new
         @order.site_id          = @site ? @site.id : nil
         @order.status           = Caboose::Order::STATUS_CART

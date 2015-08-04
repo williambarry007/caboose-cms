8933802ae6af7a19baaafdae718a539e5bf31ec8
William Barry <williambarry007@gmail.com>
2015-08-04 09:42:09 -0500

    More changes

--- app/controllers/caboose/google_spreadsheets_controller.rb
+++ app/controllers/caboose/google_spreadsheets_controller.rb
@@ -6,9 +6,15 @@ module Caboose
     # GET /google-spreadsheets/:spreadsheet_id/csv
     def csv_data           
       spreadsheet_id = params[:spreadsheet_id]                                                                                                              
-      url = "https://docs.google.com/spreadsheets/d/#{spreadsheet_id}/export?format=csv&id=#{spreadsheet_id}&gid=0"             
+      #url = "https://docs.google.com/spreadsheets/d/#{spreadsheet_id}/export?format=csv&id=#{spreadsheet_id}&gid=0"
+      url = "https://docs.google.com/spreadsheets/d/#{spreadsheet_id}/pub?output=csv&single=true&gid=0"            
       resp = HTTParty.get(url)
-      arr = CSV.parse(resp.body)                    
+      arr = nil
+      begin 
+        arr = CSV.parse(resp.body)
+      rescue
+        Caboose.log("Error parsing CSV in spreadsheet #{spreadsheet_id}:\n\n#{resp.body}")    
+      end
       render :json => arr
     end
 

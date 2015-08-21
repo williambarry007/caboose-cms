3140a2137f1ede522b09df2abb34f1cbb750e957
billyswift <billyswifty@gmail.com>
2015-08-21 11:16:23 -0500

    add color field to calendar

--- app/models/caboose/calendar.rb
+++ app/models/caboose/calendar.rb
@@ -2,6 +2,6 @@ module Caboose
   class Calendar < ActiveRecord::Base
     self.table_name = "calendars"
     has_many :calendar_events, :dependent => :destroy  
-    attr_accessible :id, :site_id, :name, :description
+    attr_accessible :id, :site_id, :name, :description, :color
   end
 end

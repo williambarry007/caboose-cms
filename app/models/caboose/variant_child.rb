4fc308511e60272a4657132c83d564359963446f
William Barry <williambarry007@gmail.com>
2016-10-25 13:09:52 -0500

    More changes

--- app/models/caboose/variant_child.rb
+++ app/models/caboose/variant_child.rb
@@ -2,8 +2,8 @@ module Caboose
   class VariantChild < ActiveRecord::Base
     self.table_name = 'store_variant_children'
     
-    belongs_to :parent, :class_name => 'Caboose::Variant'
-    belongs_to :variant, :class_name => 'Caboose::Variant'    
+    belongs_to :parent  , :class_name => 'Caboose::Variant', :foreign_key => 'parent_id'
+    belongs_to :variant , :class_name => 'Caboose::Variant', :foreign_key => 'variant_id'
     attr_accessible :id,
       :parent_id,
       :variant_id,

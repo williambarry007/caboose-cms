4249149b2c1b03d45f2f56d0f9ac1a44550be48e
billyswift <billyswifty@gmail.com>
2017-11-30 10:27:37 -0600

    More changes

--- app/controllers/caboose/sns_controller.rb
+++ app/controllers/caboose/sns_controller.rb
@@ -4,7 +4,7 @@ module Caboose
     skip_before_filter :verify_authenticity_token
 
     # @route GET /admin/sns
-    def admin_index      
+    def admin_index
       render :json => true
     end
 
@@ -12,32 +12,33 @@ module Caboose
     def admin_add
       body = JSON.parse(request.raw_post, {symbolize_names: true})
       Caboose.log(body)
-      if body[:Type] && body[:Type] == "SubscriptionConfirmation"
-        Caboose.log("SNS Subscription SubscribeURL\n#{body[:SubscribeURL]}")
-      elsif body[:Subject] == 'Amazon S3 Notification'
-        msg = JSON.parse(body[:Message])
-        if msg['Records']
-          msg['Records'].each do |r|
-            if r['eventName'] && r['eventName'].starts_with?('ObjectCreated')          
-              if r['s3'] && r['s3']['object'] && r['s3']['object']['key']
-                                
-                key = URI.decode(r['s3']['object']['key']).gsub('+', ' ')
-                Caboose.log("Processing #{key}")
-
-                arr = key.split('_')
-                media_category_id = arr.shift
-                original_name = arr.join('_')  
-                name = Caboose::Media.upload_name(original_name)
-                                                                      
-                m = Media.where(:media_category_id => media_category_id, :original_name => original_name, :name => name).first
-                m = Media.create(:media_category_id => media_category_id, :original_name => original_name, :name => name, :processed => false) if m.nil?                
-                m.delay(:queue => 'caboose_media').process
-      
-              end
-            end                  
-          end
+      # if body[:Records]
+      #   records = body[:Records]
+      #   # if body[:Type] && body[:Type] == "SubscriptionConfirmation"
+      #   #   Caboose.log("SNS Subscription SubscribeURL\n#{body[:SubscribeURL]}")
+      #   if records['eventSource'] == "aws:s3"
+      #     msg = JSON.parse(body[:Message])
+      #     if msg['Records']
+      if body && body['Records']
+        body['Records'].each do |r|
+          if r['eventName'] && r['eventName'].starts_with?('ObjectCreated')          
+            if r['s3'] && r['s3']['object'] && r['s3']['object']['key']
+              key = URI.decode(r['s3']['object']['key']).gsub('+', ' ')
+              Caboose.log("Processing #{key}")
+              arr = key.split('_')
+              media_category_id = arr.shift
+              original_name = arr.join('_')  
+              name = Caboose::Media.upload_name(original_name)                                       
+              m = Media.where(:media_category_id => media_category_id, :original_name => original_name, :name => name).first
+              m = Media.create(:media_category_id => media_category_id, :original_name => original_name, :name => name, :processed => false) if m.nil?                
+              m.delay(:queue => 'caboose_media').process
+            end
+          end                  
         end
       end
+      #     end
+      #   end
+      # end
       render :json => true
     end
     

4099f413b56a98c89a57d391786fa32cc3dce721
billyswift <billyswifty@gmail.com>
2018-01-11 14:09:57 -0600

    admin main menu

--- app/models/caboose/core_plugin.rb
+++ app/models/caboose/core_plugin.rb
@@ -3,13 +3,20 @@ class Caboose::CorePlugin < Caboose::CaboosePlugin
   def self.admin_nav(nav, user, page, site)    
     return nav if user.nil?
         
-    nav << { 'id' => 'logout'       , 'text' => 'Logout'     , 'href' => '/logout'     , 'modal' => false }    
-    nav << { 'id' => 'my-account'   , 'text' => 'My Account' , 'href' => '/my-account' , 'modal' => true }
+  #  nav << { 'id' => 'logout'       , 'text' => 'Logout'     , 'href' => '/logout'     , 'modal' => false }    
+  #  nav << { 'id' => 'my-account'   , 'text' => 'My Account' , 'href' => '/my-account' , 'modal' => true }
+
+    item = { 'id' => 'content', 'text' => 'Content', 'children' => [] }    
+    item['children'] << { 'id' => 'media'        , 'text' => 'Media'       , 'href' => '/admin/media'        , 'modal' => false } if user.is_allowed('media'       , 'view')
+    item['children'] << { 'id' => 'pages'        , 'text' => 'Pages'       , 'href' => '/admin/pages'        , 'modal' => false } if user.is_allowed('pages'       , 'view')
+    item['children'] << { 'id' => 'posts'        , 'text' => 'Posts'       , 'href' => '/admin/posts'        , 'modal' => false } if user.is_allowed('posts'       , 'view')
+    item['children'] << { 'id' => 'calendars'    , 'text' => 'Calendars'   , 'href' => '/admin/calendars'    , 'modal' => false } if user.is_allowed('calendars'   , 'view')  
+    nav << item if item['children'].count > 0
     
     item = { 'id' => 'core', 'text' => 'Settings', 'children' => [] }                        
  #   item['children'] << { 'id' => 'blocktypes'        , 'text' => 'AB Test Variants'    , 'href' => '/admin/ab-variants'        , 'modal' => false } if user.is_allowed('abvariants'        , 'view')    
     item['children'] << { 'id' => 'blocktypes'        , 'text' => 'Block Types'         , 'href' => '/admin/block-types'        , 'modal' => false } if user.is_allowed('blocktypes'        , 'view') && site.is_master == true
-    item['children'] << { 'id' => 'fonts'             , 'text' => 'Fonts'               , 'href' => '/admin/fonts'              , 'modal' => false } if user.is_allowed('fonts'             , 'view') && site.use_fonts == true
+    item['children'] << { 'icon' => 'star', 'id' => 'fonts'             , 'text' => 'Fonts'               , 'href' => '/admin/fonts'              , 'modal' => false } if user.is_allowed('fonts'             , 'view') && site.use_fonts == true
     item['children'] << { 'id' => 'redirects'         , 'text' => 'Permanent Redirects' , 'href' => '/admin/redirects'          , 'modal' => false } if user.is_allowed('redirects'         , 'view')
     item['children'] << { 'id' => 'permissions'       , 'text' => 'Permissions'         , 'href' => '/admin/permissions'        , 'modal' => false } if user.is_allowed('permissions'       , 'view') && site.is_master == true
     item['children'] << { 'id' => 'pagecustomfields'  , 'text' => 'Page Custom Fields'  , 'href' => '/admin/page-custom-fields' , 'modal' => false } if user.is_allowed('pagecustomfields'  , 'view')
@@ -19,27 +26,22 @@ class Caboose::CorePlugin < Caboose::CaboosePlugin
     item['children'] << { 'id' => 'sites'             , 'text' => 'Sites'               , 'href' => '/admin/sites'              , 'modal' => false } if user.is_allowed('sites'             , 'view') && site.is_master == true        
     item['children'] << { 'id' => 'smtp'              , 'text' => 'SMTP (Mail)'         , 'href' => '/admin/smtp'               , 'modal' => false } if user.is_allowed('smtp'              , 'view')
     item['children'] << { 'id' => 'social'            , 'text' => 'Social Media'        , 'href' => '/admin/social'             , 'modal' => false } if user.is_allowed('social'            , 'view')
-    item['children'] << { 'id' => 'store'             , 'text' => 'Store'               , 'href' => '/admin/store'              , 'modal' => false } if user.is_allowed('store'             , 'view') && site.use_store == true
     item['children'] << { 'id' => 'users'             , 'text' => 'Users'               , 'href' => '/admin/users'              , 'modal' => false } if user.is_allowed('users'             , 'view')    
-    item['children'] << { 'id' => 'subscriptions'     , 'text' => 'Subscriptions'       , 'href' => '/admin/subscriptions'      , 'modal' => false } if user.is_allowed('subscriptions'     , 'view') && site.use_store == true
-   # item['children'] << { 'id' => 'variables'         , 'text' => 'Variables'           , 'href' => '/admin/settings'           , 'modal' => false } if user.is_allowed('settings'          , 'view')    
+   # item['children'] << { 'id' => 'variables'         , 'text' => 'Variables'           , 'href' => '/admin/settings'           , 'modal' => false } if user.is_allowed('settings'          , 'view')   
+    item['children'] << { 'id' => 'my-account'             , 'text' => 'My Account'               , 'href' => '/my-account'              , 'modal' => false } 
     nav << item if item['children'].count > 0
-    
-    item = { 'id' => 'content', 'text' => 'Content', 'children' => [] }    
-    item['children'] << { 'id' => 'media'        , 'text' => 'Media'       , 'href' => '/admin/media'        , 'modal' => false } if user.is_allowed('media'       , 'view')
-    item['children'] << { 'id' => 'pages'        , 'text' => 'Pages'       , 'href' => '/admin/pages'        , 'modal' => false } if user.is_allowed('pages'       , 'view')
-    item['children'] << { 'id' => 'posts'        , 'text' => 'Posts'       , 'href' => '/admin/posts'        , 'modal' => false } if user.is_allowed('posts'       , 'view')
-    item['children'] << { 'id' => 'calendars'    , 'text' => 'Calendars'   , 'href' => '/admin/calendars'    , 'modal' => false } if user.is_allowed('calendars'   , 'view')  
-    nav << item if item['children'].count > 0
+
     
     if site.use_store      
       item = { 'id' => 'store', 'text' => 'Store', 'children' => [] }
-      item['children'] << { 'id' => 'categories'       , 'href' => '/admin/categories'        , 'text' => 'Categories'        , 'modal' => false } if user.is_allowed('categories'       , 'view')
-      item['children'] << { 'id' => 'giftcards'        , 'href' => '/admin/gift-cards'        , 'text' => 'Gift Cards'        , 'modal' => false } if user.is_allowed('giftcards'        , 'view')
-      item['children'] << { 'id' => 'invoices'         , 'href' => '/admin/invoices'          , 'text' => 'Invoices'          , 'modal' => false } if user.is_allowed('invoices'         , 'view')
-      item['children'] << { 'id' => 'products'         , 'href' => '/admin/products'          , 'text' => 'Products'          , 'modal' => false } if user.is_allowed('products'         , 'view')
-      item['children'] << { 'id' => 'shippingpackages' , 'href' => '/admin/shipping-packages' , 'text' => 'Shipping Packages' , 'modal' => false } if user.is_allowed('shippingpackages' , 'view')
-      item['children'] << { 'id' => 'vendors'          , 'href' => '/admin/store/vendors'     , 'text' => 'Vendors'           , 'modal' => false } if user.is_allowed('vendors'          , 'view')    
+      item['children'] << { 'icon' => 'tags', 'id' => 'categories'       , 'href' => '/admin/categories'        , 'text' => 'Categories'        , 'modal' => false } if user.is_allowed('categories'       , 'view')
+      item['children'] << { 'icon' => 'paper', 'id' => 'giftcards'        , 'href' => '/admin/gift-cards'        , 'text' => 'Gift Cards'        , 'modal' => false } if user.is_allowed('giftcards'        , 'view')
+      item['children'] << { 'icon' => 'money', 'id' => 'invoices'         , 'href' => '/admin/invoices'          , 'text' => 'Invoices'          , 'modal' => false } if user.is_allowed('invoices'         , 'view')
+      item['children'] << { 'icon' => 'store', 'id' => 'products'         , 'href' => '/admin/products'          , 'text' => 'Products'          , 'modal' => false } if user.is_allowed('products'         , 'view')
+      item['children'] << { 'icon' => 'dropbox', 'id' => 'shippingpackages' , 'href' => '/admin/shipping-packages' , 'text' => 'Shipping Packages' , 'modal' => false } if user.is_allowed('shippingpackages' , 'view')
+      item['children'] << { 'icon' => 'roles', 'id' => 'vendors'          , 'href' => '/admin/store/vendors'     , 'text' => 'Vendors'           , 'modal' => false } if user.is_allowed('vendors'          , 'view')    
+      item['children'] << { 'icon' => 'reload', 'id' => 'subscriptions'     , 'text' => 'Subscriptions'       , 'href' => '/admin/subscriptions'      , 'modal' => false } if user.is_allowed('subscriptions'         , 'view')
+      item['children'] << { 'icon' => 'settings', 'id' => 'store'             , 'text' => 'Store Settings'               , 'href' => '/admin/store'              , 'modal' => false } if user.is_allowed('store'             , 'view')
       nav << item if item['children'].count > 0
     end
                 
